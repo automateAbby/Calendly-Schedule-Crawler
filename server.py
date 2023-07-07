c4285768e3cdd26add170c84af268e7dee61a3c7
automateAbby <138645677+automateAbby@users.noreply.github.com>
2023-07-07 17:55:04 +0800

    Update server.py

--- server.py
+++ server.py
@@ -16,12 +16,10 @@ chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
-browser = webdriver.Chrome(options=chrome_options)
+browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
 
 origins = [
-   "https://testapi.consulting.repl.co",
-   "https://www.greenlyai.space",
-   "https://greenlyai.space"
+   "*"
 ]
 
 middleware = [
