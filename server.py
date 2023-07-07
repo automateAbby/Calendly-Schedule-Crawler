0fdf59922c272fa0227ca184e8a1815d1d4cb61d
automateAbby <138645677+automateAbby@users.noreply.github.com>
2023-07-07 17:28:53 +0800

    Update server.py

--- server.py
+++ server.py
@@ -73,4 +73,4 @@ async def schedule(schedule_data: Schedule):
     return 'SUCCESS'
 
 if __name__ == "__main__":
-	uvicorn.run(app, host='0.0.0.0', port=os.environ.get('PORT', '5000'))
+	print('system running')
