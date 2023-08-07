983c0e1568ab2226c6aa35bbf3d2b69f4affb5a2
automateAbby <138645677+automateAbby@users.noreply.github.com>
2023-08-07 21:09:03 +0800

    Update server.py

--- server.py
+++ server.py
@@ -90,7 +90,7 @@ async def schedule(schedule_data: Schedule):
     browser.implicitly_wait(15)
     browser.find_element(By.ID, "email_input").send_keys(email)
     browser.find_element(By.ID,"full_name_input").send_keys(name)
-    return browser.getCurrentUrl()
+    return browser.current_url
 
 if __name__ == "__main__":
 	uvicorn.run(app, host='0.0.0.0', port=os.environ.get('PORT', '5000'))
